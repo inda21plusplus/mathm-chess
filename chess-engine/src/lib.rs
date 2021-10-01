3d6de7b7a73a12ab90e527831a568eec211d7676
Mathias <mathm@kth.se>
2021-10-01 22:08:08 +0200

    Ditch decider trait into the trench

--- chess-engine/src/lib.rs
+++ chess-engine/src/lib.rs
@@ -1,16 +1,14 @@
-//! For example uses, see `Decider`, and `Game`
+//! For example uses, see `Game`
 
 #![deny(warnings)]
 
 mod board;
-mod decider;
 mod error;
 mod game;
 pub mod piece;
 mod util;
 
 pub use board::Board;
-pub use decider::Decider;
 pub use error::Error;
 pub use game::{Game, GameState};
 pub use piece::Piece;
